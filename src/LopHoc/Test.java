2d6e1cf24321fab873aa48df2d689f8b90e1e648
Phuoc <phuoc11102004@gmail.com>
2024-04-06 11:02:16 +0700

    update Temp

--- src/LopHoc/Test.java
+++ src/LopHoc/Test.java
@@ -2,7 +2,7 @@ package LopHoc;
 
 public class Test {
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i <12; i++)
 			System.out.println("Test Code");
 	}
 }
