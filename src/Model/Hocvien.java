6bfc58b6a48ac499729f270b99ce2616afa0fbd8
Phuoc <phuoc11102004@gmail.com>
2024-04-18 15:19:26 +0700

    hocvien

--- src/Model/Hocvien.java
+++ src/Model/Hocvien.java
@@ -3,6 +3,20 @@ package Model;
 import java.sql.Date;
 
 public class Hocvien {
+	
+	public Hocvien(String maHV, String name, Date namSinh, boolean gioiTinh, String sdt, String tinhTrang) {
+		super();
+		this.maHV = maHV;
+		this.name = name;
+		this.namSinh = namSinh;
+		this.gioiTinh = gioiTinh;
+		this.sdt = sdt;
+		this.tinhTrang = tinhTrang;
+	}
+	
+	public Hocvien() {
+		super();
+	}
 	private String maHV;
 	private String name;
 	private Date namSinh;
@@ -10,40 +24,54 @@ public class Hocvien {
 	private String sdt;
 	private String tinhTrang;
 	
+	//Getter setter
 	public String getMaHV() {
 		return maHV;
 	}
+
 	public void setMaHV(String maHV) {
 		this.maHV = maHV;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Date getNamSinh() {
 		return namSinh;
 	}
+
 	public void setNamSinh(Date namSinh) {
 		this.namSinh = namSinh;
 	}
+
 	public boolean isGioiTinh() {
 		return gioiTinh;
 	}
+
 	public void setGioiTinh(boolean gioiTinh) {
 		this.gioiTinh = gioiTinh;
 	}
+
 	public String getSdt() {
 		return sdt;
 	}
+
 	public void setSdt(String sdt) {
 		this.sdt = sdt;
 	}
+
 	public String getTinhTrang() {
 		return tinhTrang;
 	}
+
 	public void setTinhTrang(String tinhTrang) {
 		this.tinhTrang = tinhTrang;
 	}
+	
+	
 }
