8166842cbb066c23e0da8ab1b59d7e51449fe6a6
Phuoc <phuoc11102004@gmail.com>
2024-04-18 13:51:57 +0700

    get set hocvien

--- src/Model/Lophoc.java
+++ src/Model/Lophoc.java
@@ -4,48 +4,5 @@ import java.sql.Date;
 
 public class Lophoc {
 	
-		private String maHV;
-		private String name;
-		private Date namSinh;
-		private boolean gioiTinh;
-		private String sdt;
-		private String tinhTrang;
-		
-		public String getMaHV() {
-			return maHV;
-		}
-		public void setMaHV(String maHV) {
-			this.maHV = maHV;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public Date getNamSinh() {
-			return namSinh;
-		}
-		public void setNamSinh(Date namSinh) {
-			this.namSinh = namSinh;
-		}
-		public boolean isGioiTinh() {
-			return gioiTinh;
-		}
-		public void setGioiTinh(boolean gioiTinh) {
-			this.gioiTinh = gioiTinh;
-		}
-		public String getSdt() {
-			return sdt;
-		}
-		public void setSdt(String sdt) {
-			this.sdt = sdt;
-		}
-		public String getTinhTrang() {
-			return tinhTrang;
-		}
-		public void setTinhTrang(String tinhTrang) {
-			this.tinhTrang = tinhTrang;
-		}
 		
 }
