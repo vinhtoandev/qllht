e5c85f882ae1e0fb6d0f5f0872aeabede4c2df4c
Phuoc <phuoc11102004@gmail.com>
2024-04-19 13:04:58 +0700

    chinh sua

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -12,7 +12,7 @@ public class Main {
 	public static void main(String[] args) {
 		Connection cnn = JDBCUtil.getConnection();
 		Hocvien hv1 = new Hocvien("10004", "lvup", "2004-9-2", false, "0334896572", "active");
-		HocvienDAO.getInstance().update(hv1);
+		HocvienDAO.getInstance().delete(hv1);
 		JDBCUtil.closeConnection(cnn);
 	}
 	
